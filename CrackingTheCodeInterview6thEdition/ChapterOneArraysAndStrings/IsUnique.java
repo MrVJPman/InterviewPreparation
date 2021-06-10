6789b66877616f771950994080ef73eb50bbba36
unknown <bob.kong@mail.utoronto.ca>
2021-06-09 21:18:09 -0400

    Finished isUnique Attempt

---
+++ CrackingTheCodeInterview6thEdition/ChapterOneArraysAndStrings/IsUnique.java
@@ -0,0 +1,29 @@
+class Main {
+  
+  //Code won't work for UNICODE 
+  private static boolean isUnique(String inputString) {
+
+    //If the size of the array is greater than 256
+    //By default the string will be non-unique as it will be guarantee to have the same character
+    if (inpuString.length() > 256) {
+      return false
+    }
+
+
+    boolean [] characterExist = new boolean[256];
+    for (char c: inputString.toCharArray()) {
+      if (characterExist[(int)c]) {
+        return false;
+      }
+      else {
+        characterExist[(int)c] = true;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isUnique("world!"));
+    System.out.println(isUnique("Hello world!"));
+  }
+}
